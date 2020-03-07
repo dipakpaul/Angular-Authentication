a79377698ff46418bd7245471e873199cb6d44c4
DIPAK PAUL <dipakpaul@DIPAK-PAULs-MacBook-Pro.local>
2020-03-07 19:44:30 +0530

    added initial commit with register

---
+++ ngApp/src/app/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _registrationUrl = "http://localhost:3000/api/register"
+  constructor(private _http: HttpClient) { }
+  
+  registerUser(user) {
+    return this._http.post<any>(this._registrationUrl, user)
+  }
+}
